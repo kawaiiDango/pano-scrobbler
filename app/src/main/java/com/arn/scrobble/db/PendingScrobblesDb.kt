b29c04c5cecb87d74e24f6ac472a6cb4e2eab718
kawaiiDango <36arnab@gmail.com>
2017-11-26 19:48:46 +0530

    fix db leak

--- app/src/main/java/com/arn/scrobble/db/PendingScrobblesDb.kt
+++ app/src/main/java/com/arn/scrobble/db/PendingScrobblesDb.kt
@@ -32,6 +32,8 @@ abstract class PendingScrobblesDb : RoomDatabase() {
         }
 
         fun destroyInstance() {
+            if (INSTANCE?.isOpen == true)
+                INSTANCE?.close()
             INSTANCE = null
         }
 

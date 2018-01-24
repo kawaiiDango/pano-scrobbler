2404e054570a91c24b03556b43b36f3ac1e31715
kawaiiDango <36arnab@gmail.com>
2018-01-25 00:52:52 +0530

    faster processing of offline scrobbles

--- app/src/main/java/com/arn/scrobble/OfflineScrobbleJob.kt
+++ app/src/main/java/com/arn/scrobble/OfflineScrobbleJob.kt
@@ -8,6 +8,7 @@ import android.content.ComponentName
 import android.content.Context
 import android.os.Build
 import android.preference.PreferenceManager
+import com.arn.scrobble.db.PendingScrobblesDao
 import com.arn.scrobble.db.PendingScrobblesDb
 import de.umass.lastfm.Session
 import de.umass.lastfm.Track
@@ -33,12 +34,11 @@ class OfflineScrobbleJob : JobService() {
         override fun run() {
             var done = true
             val dao = PendingScrobblesDb.getDb(this@OfflineScrobbleJob).getDao()
-            val count = dao.count
 
-            while (dao.allNotAutocorrectedCount > 0 && dao.allAutocorrectedCount < BATCH_SIZE){
+            while (dao.allNotAutocorrectedCount > 0){
                 val entry = dao.loadLastPending
-                Stuff.log("db count: " + count)
-                Stuff.log(entry.toString())
+
+                Stuff.log("offline proc: " + entry.toString())
                 var corrected: Pair<String,String>?
                 try {
                     corrected = LFMRequester.getCorrectedData(entry.artist, entry.track)
@@ -47,59 +47,65 @@ class OfflineScrobbleJob : JobService() {
                     done = false
                     break
                 }
-                if (corrected != null) {
-                    entry.artist = corrected.first
-                    entry.track = corrected.second
-                    entry.autoCorrected = 1
-                    dao.update(entry)
-                } else
-                    dao.delete(entry) //invalid
-                Thread.sleep(800)
+                if (corrected != null)
+                    dao.markValidArtist(entry.artist)
+                else
+                    dao.deleteInvalidArtist(entry.artist)
+                Thread.sleep(100)
+
+                if (dao.allAutocorrectedCount >= BATCH_SIZE)
+                    done = submitBatch(dao)
             }
 
+            if (dao.allAutocorrectedCount > 0)
+                done = submitBatch(dao)
+
+            Stuff.log("jobFinished: " + done)
+            jobFinished(jp, !done)
+        }
+
+        private fun submitBatch(dao: PendingScrobblesDao):Boolean {
+            var done = true
+            val entries = dao.allAutocorrected(BATCH_SIZE)
             val key: String? = PreferenceManager.getDefaultSharedPreferences(applicationContext)
                     .getString(Stuff.SESS_KEY, null) //user decided to log out
             if (key == null) {
 //                done = true
                 jobFinished(jp, !done)
-                return
+                return done
             }
-            if (done) {
-                val entries = dao.allAutocorrected(BATCH_SIZE)
-                val session = Session.createSession(Stuff.LAST_KEY, Stuff.LAST_SECRET, key)
-                val scrobbleDatas = mutableListOf<ScrobbleData>()
-                entries.forEach {
-                    val scrobbleData = ScrobbleData()
-                    scrobbleData.artist = it.artist
-                    scrobbleData.album = it.album
-                    scrobbleData.track = it.track
-                    scrobbleData.timestamp = (it.timestamp / 1000).toInt() // in secs
-                    if(it.duration > 10*1000)
-                        scrobbleData.duration = (it.duration / 1000).toInt() // in secs
-                    scrobbleDatas.add(scrobbleData)
-                }
-                if (scrobbleDatas.isNotEmpty()) {
-                    try {
-                        val scrobbleResults = Track.scrobble(scrobbleDatas, session)
-                        scrobbleResults.forEachIndexed { i, it ->
-                            if (it != null && (it.isSuccessful || it.isIgnored)) {
-                                dao.delete(entries[i])
-                            } else {
-                                done = false
-                                entries[i].state_timestamp = System.currentTimeMillis()
-                                entries[i].state = STATE_SCROBBLE_ERR
-                                dao.update(entries[i])
-                            }
+            val session = Session.createSession(Stuff.LAST_KEY, Stuff.LAST_SECRET, key)
+            val scrobbleDatas = mutableListOf<ScrobbleData>()
+            entries.forEach {
+                val scrobbleData = ScrobbleData()
+                scrobbleData.artist = it.artist
+                scrobbleData.album = it.album
+                scrobbleData.track = it.track
+                scrobbleData.timestamp = (it.timestamp / 1000).toInt() // in secs
+                if(it.duration > 10*1000)
+                    scrobbleData.duration = (it.duration / 1000).toInt() // in secs
+                scrobbleDatas.add(scrobbleData)
+            }
+            if (scrobbleDatas.isNotEmpty()) {
+                try {
+                    val scrobbleResults = Track.scrobble(scrobbleDatas, session)
+                    scrobbleResults.forEachIndexed { i, it ->
+                        if (it != null && (it.isSuccessful || it.isIgnored)) {
+                            dao.delete(entries[i])
+                        } else {
+                            done = false
+                            entries[i].state_timestamp = System.currentTimeMillis()
+                            entries[i].state = STATE_SCROBBLE_ERR
+                            dao.update(entries[i])
                         }
-
-                    } catch (e: Exception) {
-                        Stuff.log("job scrobble n/w err: " + e.message)
-                        done = false
                     }
+
+                } catch (e: Exception) {
+                    Stuff.log("job scrobble n/w err: " + e.message)
+                    done = false
                 }
             }
-            Stuff.log("jobFinished: " + done)
-            jobFinished(jp, !done)
+            return done
         }
     }
 
@@ -121,7 +127,7 @@ class OfflineScrobbleJob : JobService() {
 
             val job = JobInfo.Builder(OfflineScrobbleJob.JOB_ID, ComponentName(context, OfflineScrobbleJob::class.java))
                     .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                    .setMinimumLatency(30*1000)
+                    .setMinimumLatency(20*1000)
                     .setPersisted(true)
                     .build()
             val jsRet = js.schedule(job)
--- app/src/main/java/com/arn/scrobble/db/PendingScrobblesDao.kt
+++ app/src/main/java/com/arn/scrobble/db/PendingScrobblesDao.kt
@@ -19,6 +19,12 @@ interface PendingScrobblesDao {
     @Query("SELECT * FROM $tableName WHERE autoCorrected = 0 LIMIT :limit")
     fun allNotAutocorrected(limit: Int): List<PendingScrobble>
 
+    @Query("UPDATE $tableName SET autoCorrected = 1 WHERE ARTIST = :artist")
+    fun markValidArtist(artist: String)
+
+    @Query("DELETE FROM $tableName WHERE ARTIST = :artist")
+    fun deleteInvalidArtist(artist: String)
+
     @get:Query("SELECT * FROM $tableName WHERE autoCorrected = 0 LIMIT 1")
     val loadLastPending: PendingScrobble
 

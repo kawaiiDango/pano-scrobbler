f12f1fa98902e7b64062127747f1b1a48170b5c1
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2021-05-07 00:27:29 +0530

    splash screen

--- app/src/main/java/com/arn/scrobble/DummyFragment.kt
+++ app/src/main/java/com/arn/scrobble/DummyFragment.kt
@@ -1,10 +1,13 @@
 package com.arn.scrobble
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
+import android.os.Handler
 import android.transition.Slide
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.arn.scrobble.pref.MultiPreferences
 
@@ -12,18 +15,13 @@ import com.arn.scrobble.pref.MultiPreferences
 /**
  * Created by arn on 06/09/2017.
  */
-class DummyFragment: Fragment() {
+class DummyFragment: Fragment(R.layout.content_avd_test) {
     private lateinit var pref: MultiPreferences
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-//        exitTransition = Slide()
-//        enterTransition = Slide()
-        returnTransition = Slide()
-//        reenterTransition = Slide()
-
-        val view = inflater.inflate(R.layout.content_login, container, false)
-
-        return view
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val avd = view.findViewById<ImageView>(R.id.test_avd).drawable as AnimatedVectorDrawable
+        Handler().postDelayed({ avd.start() }, 1000)
     }
 
     override fun onStart() {

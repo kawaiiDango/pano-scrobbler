f47f0d9a5c138578c580942ff3efdf3371002fb3
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2022-11-25 19:34:16 +0530

    implement artwork censoring for screenshots

---
+++ app/src/main/java/com/arn/scrobble/ui/CrystallizeTransformation.kt
@@ -0,0 +1,54 @@
+@file:Suppress("unused")
+
+package com.arn.scrobble.ui
+
+import android.content.Context
+import android.graphics.Bitmap
+import coil.size.Size
+import coil.transform.Transformation
+import com.arn.scrobble.ui.UiUtils.dp
+import com.jabistudio.androidjhlabs.filter.CrystallizeFilter
+import com.jabistudio.androidjhlabs.filter.util.AndroidUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class CrystallizeTransformation constructor(
+    private val context: Context,
+) : Transformation {
+
+    override val cacheKey = "${CrystallizeTransformation::class.java.name}-a"
+
+    override suspend fun transform(input: Bitmap, size: Size): Bitmap =
+        withContext(Dispatchers.IO) {
+            val outArr = CrystallizeFilter().apply {
+                scale = 15.dp.toFloat()
+                randomness = 0.01f
+                edgeThickness = 0f
+            }.filter(
+                AndroidUtils.bitmapToIntArray(input),
+                input.width,
+                input.height
+            )
+            Bitmap.createBitmap(
+                outArr,
+                input.width,
+                input.height,
+                Bitmap.Config.ARGB_8888
+            )
+        }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        return other is CrystallizeTransformation &&
+                context == other.context
+    }
+
+    override fun hashCode(): Int {
+        return context.hashCode()
+    }
+
+    override fun toString(): String {
+        return "CrystallizeTransformation(context=$context)"
+    }
+}
--- app/src/main/java/com/arn/scrobble/ui/DemoInterceptor.kt
+++ app/src/main/java/com/arn/scrobble/ui/DemoInterceptor.kt
@@ -8,10 +8,12 @@ class DemoInterceptor : Interceptor {
 
     override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
         val imgUrl = chain.request.data as? String
-        return if (imgUrl != null && (imgUrl.endsWith(".webp") || imgUrl.endsWith(".gif"))) {
+        return if (imgUrl != null &&
+            (imgUrl.endsWith(".webp") || imgUrl.endsWith(".gif") || imgUrl.startsWith("https://i.scdn.co"))
+        ) {
             chain.proceed(
                 ImageRequest.Builder(chain.request)
-                    .transformations(BlurTransformation(chain.request.context, 25f, 1.5f))
+                    .transformations(CrystallizeTransformation(chain.request.context))
                     .build()
             )
         } else

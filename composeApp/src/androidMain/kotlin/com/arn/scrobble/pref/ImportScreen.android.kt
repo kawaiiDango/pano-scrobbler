61388ec6c2bdb90b1caa7a7218ee98e3c4f47767
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2026-04-06 17:06:25 +0530

    show dialog before permission prompt

--- composeApp/src/androidMain/kotlin/com/arn/scrobble/pref/ImportScreen.android.kt
+++ composeApp/src/androidMain/kotlin/com/arn/scrobble/pref/ImportScreen.android.kt
@@ -7,7 +7,15 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.arn.scrobble.ui.AlertDialogOk
 import com.arn.scrobble.utils.AndroidStuff
+import org.jetbrains.compose.resources.stringResource
+import pano_scrobbler.composeapp.generated.resources.Res
+import pano_scrobbler.composeapp.generated.resources.missing_local_network_permission
 
 @Composable
 actual fun ImportScreenPermissionsRequest(
@@ -26,15 +34,27 @@ actual fun ImportScreenPermissionsRequest(
     } else
         null
 
+    var showDialog by remember { mutableStateOf(false) }
+
     LaunchedEffect(Unit) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CINNAMON_BUN &&
             AndroidStuff.applicationContext.checkSelfPermission(
                 Manifest.permission.ACCESS_LOCAL_NETWORK
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            permRequest?.launch(Manifest.permission.ACCESS_LOCAL_NETWORK)
+            showDialog = true
         } else {
             onGranted()
         }
+    }
+
+    if (showDialog && Build.VERSION.SDK_INT >= Build.VERSION_CODES.CINNAMON_BUN) {
+        AlertDialogOk(
+            text = stringResource(Res.string.missing_local_network_permission),
+            onConfirmation = {
+                showDialog = false
+                permRequest?.launch(Manifest.permission.ACCESS_LOCAL_NETWORK)
+            }
+        )
     }
 }

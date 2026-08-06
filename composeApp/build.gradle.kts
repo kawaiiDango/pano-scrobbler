6433bd0bbcd4930aea4bc9a889cf3177f1333b4c
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2026-08-07 04:47:32 +0530

    isccPathUser

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -332,13 +332,14 @@ tasks.register<Exec>("packageInno") {
     val scriptFile = file("inno/installer.iss")
     val iconFile = file("app-icons/pano-scrobbler.ico")
     val isccPath = System.getenv("PROGRAMFILES") + "\\Inno Setup 7\\ISCC.exe"
+    val isccPathUser = System.getenv("LOCALAPPDATA") + "\\Programs\\Inno Setup 7\\ISCC.exe"
 
     doFirst {
         distDir.mkdirs()
     }
 
     commandLine(
-        isccPath,
+        if (File(isccPath).exists()) isccPath else isccPathUser,
         "/DOUT_DIR=" + distDir.absolutePath,
         "/DAPP_DIR=" + executableDir.absolutePath,
         "/DVERSION=$VER_NAME",

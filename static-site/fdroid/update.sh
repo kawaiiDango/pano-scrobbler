0e2bd0c2fcc961fd513ef69c12fad64b05c5d4a7
kawaiiDango <1066519+kawaiiDango@users.noreply.github.com>
2026-09-13 21:44:16 +0530

    Update update.sh

--- static-site/fdroid/update.sh
+++ static-site/fdroid/update.sh
@@ -6,8 +6,14 @@ PACKAGE=com.arn.scrobble
 # Define the URL of the GitHub repository
 REPO_URL="https://api.github.com/repos/$USER/$REPO/releases"
 
+CURL_ARGS=(--silent --fail-with-body)
+if [ -n "$GH_TOKEN" ]; then
+  CURL_ARGS+=(-H "Authorization: Bearer $GH_TOKEN")
+fi
+CURL_ARGS+=(-H "Accept: application/vnd.github+json")
+
 # Use curl to get the JSON response for all releases
-JSON=$(curl --silent --fail-with-body "$REPO_URL")
+JSON=$(curl "${CURL_ARGS[@]}" "$REPO_URL")
 
 # Extract the URLs of the assets named "pano-scrobbler-release.apk", the corresponding tags, the created_at dates
 readarray -t TAGS < <(jq -r '.[0:3][] | .tag_name' <<<"$JSON")
